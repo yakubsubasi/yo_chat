84a33b106883e4e019ff795b51d65b403abc7615
yakubsubasi <yakub5896@gmail.com>
2022-05-16 23:46:43 +0300

    country picker eklendi, login page ui iyileştirmeleri yapıldı

--- lib/pages/login_page/login_page.controller.dart
+++ lib/pages/login_page/login_page.controller.dart
@@ -1,11 +1,38 @@
+import 'package:country_calling_code_picker/country.dart';
+import 'package:country_calling_code_picker/functions.dart';
+import 'package:country_calling_code_picker/picker.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:yo_chat/controllers/authentication_controller.dart';
 
 class LoginPageController extends GetxController {
   final phoneNumber = ''.obs;
+  final phoneNumberError = RxnString();
+
+  late final Country selectedCountry;
+
+  void onCountryPickerPressed() async {
+    final country = Get.to(PickerPage());
+  }
 
   void login() {
     final authController = Get.find<AuthenticationController>();
     authController.login(phoneNumber.value);
+  }
+}
+
+class PickerPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Select Country'),
+      ),
+      body: Container(
+        child: CountryPickerWidget(
+          onSelected: (country) => Navigator.pop(context, country),
+        ),
+      ),
+    );
   }
 }
--- lib/pages/login_page/login_page.dart
+++ lib/pages/login_page/login_page.dart
@@ -1,5 +1,5 @@
+import 'package:country_calling_code_picker/picker.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:yo_chat/pages/login_page/login_page.controller.dart';
 
@@ -10,27 +10,56 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final controller = Get.put<LoginPageController>(LoginPageController());
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Yo App'),
-        ),
-        body: Column(
-          children: [
-            const Text('Login Page'),
-            TextField(
-              decoration: const InputDecoration(
-                labelText: 'Phone Number',
+      appBar: AppBar(
+        title: const Text('Verify phone number'),
+      ),
+      body: SingleChildScrollView(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            children: [
+              SizedBox(height: 100),
+              Obx(
+                () => Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        controller.onCountryPickerPressed();
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('Pick Country'),
+                          Icon(Icons.arrow_drop_down)
+                        ],
+                      ),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        errorText: controller.phoneNumberError.value,
+                        labelText: 'Phone Number',
+                      ),
+                      keyboardType: TextInputType.phone,
+                      onChanged: (value) {
+                        controller.phoneNumber.value = value;
+                      },
+                    ),
+                    SizedBox(
+                      height: 150,
+                    ),
+                    TextButton(
+                        onPressed: () {},
+                        child: Text('Next'),
+                        style: ButtonStyle(
+                            foregroundColor:
+                                MaterialStateProperty.all(Colors.white),
+                            backgroundColor:
+                                MaterialStateProperty.all<Color>(Colors.teal))),
+                  ],
+                ),
               ),
-              keyboardType: TextInputType.phone,
-              onChanged: (value) {
-                controller.phoneNumber.value = value;
-              },
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  controller.login();
-                },
-                child: const Text('Login'))
-          ],
-        ));
+            ],
+          )),
+    );
   }
 }
